919a8dc21f56c8fcd92447e25fdb7cc3dab62ccd
yanchunhao <yanchunhao@chelun.com>
2015-12-17 10:47:17 +0800

    1

--- libs/Swoole/CLPack.php
+++ libs/Swoole/CLPack.php
@@ -28,7 +28,7 @@ class CLPack {
 		if ($this->len && $this->count >= $this->len) {
 			$r = @json_decode(substr($this->data, self::LEN_BYTE, $this->len - self::LEN_BYTE), 1);
 			if (json_last_error() == JSON_ERROR_NONE) {
-				$this->reset(1);
+				$this->reset(0);
 				return $r;
 			}
 			$this->last_err = "json解包失败	{$this->data}\n";
