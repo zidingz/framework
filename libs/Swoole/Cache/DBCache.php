17ea2b86972dc7c82c06f6b85172d1df17364288
matyhtf <mikan.tenny@gmail.com>
2016-01-04 10:09:28 +0800

    Redis 缓存

--- libs/Swoole/Cache/DBCache.php
+++ libs/Swoole/Cache/DBCache.php
@@ -1,22 +1,32 @@
 <?php
 namespace Swoole\Cache;
+use Swoole;
 /**
  * 数据库缓存
  * @author Tianfeng.Han
  * @package Swoole
  * @subpackage cache
  */
-class DBCache implements \Swoole\IFace\Cache
+class DBCache implements Swoole\IFace\Cache
 {
     public $swoole;
     public $shard_id = 0;
 
+    /**
+     * @var Swoole\Model
+     */
+    protected $model;
+
     function __construct($table)
     {
         //用URL配置缓存
-        if(is_array($table)) $table = $table['params']['table'];
+        if (is_array($table))
+        {
+            $table = $table['params']['table'];
+        }
+
         global $php;
-        $this->model = new \Swoole\Model($php);
+        $this->model = new Swoole\Model($php);
         $this->model->table = $table;
         $this->model->create_sql = "CREATE TABLE `{$table}` (
             `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
@@ -25,17 +35,17 @@ class DBCache implements \Swoole\IFace\Cache
             `sid` INT NOT NULL ,
             `expire` INT NOT NULL ,
             INDEX ( `ckey` )
-            ) ENGINE = MYISAM ;";
+            ) ENGINE = INNODB;";
     }
 
-    function create_table()
+    function createTable()
     {
-        $this->model->create_table();
+        $this->model->createTable();
     }
 
     function shard($id)
     {
-        $this->shard_id= $id;
+        $this->shard_id = $id;
     }
 
     function gets($key_like)
@@ -58,13 +68,26 @@ class DBCache implements \Swoole\IFace\Cache
         $gets['sid'] = $this->shard_id;
         $gets['order'] = '';
         $gets['select'] = 'id,ckey,cvalue,expire';
-        $gets['in'] = array('ckey','"'.implode('","',$params).'"');
+        $gets['in'] = array('ckey', '"' . implode('","', $params) . '"');
         $list = $this->model->gets($gets);
-        foreach($list as $li)
+        foreach ($list as $li)
         {
             $return[$li['ckey']] = $this->_filter_expire($li);
         }
         return $return;
+    }
+
+    private function _filter_expire($rs)
+    {
+        if ($rs['expire'] != 0 and $rs['expire'] < time())
+        {
+            $this->model->del($rs['id']);
+            return false;
+        }
+        else
+        {
+            return $rs['cvalue'];
+        }
     }
 
     function get($key)
@@ -78,25 +101,27 @@ class DBCache implements \Swoole\IFace\Cache
         if(empty($rs)) return false;
         return $this->_filter_expire($rs[0]);
     }
-    private function _filter_expire($rs)
-    {
-        if($rs['expire']!=0 and $rs['expire']<time())
-        {
-            $this->model->del($rs['id']);
-            return false;
-        }
-        else return $rs['cvalue'];
-    }
-    function set($key,$value,$expire=0)
+
+    function set($key, $value, $expire = 0)
     {
         $in['ckey'] = $key;
-        if(is_array($value)) $value = serialize($value);
+        if (is_array($value))
+        {
+            $value = serialize($value);
+        }
         $in['cvalue'] = $value;
-        if($expire==0) $in['expire'] = $expire;
-        else $in['expire'] = time() + $expire;
+        if ($expire == 0)
+        {
+            $in['expire'] = $expire;
+        }
+        else
+        {
+            $in['expire'] = time() + $expire;
+        }
         $in['sid'] = $this->shard_id;
         $this->model->put($in);
     }
+
     function delete($key)
     {
         $gets['sid'] = $this->shard_id;
