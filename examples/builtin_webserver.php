6d09a06ca2469e67f3ba921f3a1cf1608d7d1886
matyhtf <mikan.tenny@gmail.com>
2016-01-08 11:10:16 +0800

    更新例子程序

--- examples/builtin_webserver.php
+++ examples/builtin_webserver.php
@@ -3,11 +3,19 @@ define('DEBUG', 'on');
 define('WEBPATH', dirname(__DIR__));
 require WEBPATH . '/libs/lib_config.php';
 
-$config = array(
-    'document_root' => WEBPATH,
-    'worker_num' => 100,
-    'max_request' => 5000,
-    'log_file' => '/tmp/swoole.log',
-);
-
-Swoole::$php->runHttpServer('0.0.0.0', 9501, $config);
+//设置PID文件的存储路径
+Swoole\Network\Server::setPidFile(__DIR__ . '/built_webserver.pid');
+/**
+ * 显示Usage界面
+ * php app_server.php start|stop|reload
+ */
+Swoole\Network\Server::start(function ()
+{
+    $config = array(
+        'document_root' => WEBPATH,
+        'worker_num' => 100,
+        'max_request' => 5000,
+        'log_file' => '/tmp/swoole.log',
+    );
+    Swoole::$php->runHttpServer('0.0.0.0', 9501, $config);
+});
