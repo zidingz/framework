1d52b01ab4cda744b797c5e806a99a06ca2a449e
matyhtf <mikan.tenny@gmail.com>
2016-03-14 15:41:21 +0800

    修改EchoServer代码

--- examples/echo_server.php
+++ examples/echo_server.php
@@ -7,13 +7,24 @@ Swoole\Config::$debug = false;
 
 class EchoServer extends Swoole\Protocol\Base
 {
-    function onReceive($server,$client_id, $from_id, $data)
+    function onReceive($server, $client_id, $from_id, $data)
     {
-        $this->server->send($client_id, "Swoole: ".$data);
+        $this->server->send($client_id, "Swoole: " . $data);
     }
 }
 
-$AppSvr = new EchoServer();
-$server = Swoole\Network\Server::autoCreate('0.0.0.0', 9505);
-$server->setProtocol($AppSvr);
-$server->run(array('worker_num' => 1));
+//设置PID文件的存储路径
+Swoole\Network\Server::setPidFile(__DIR__ . '/app_server.pid');
+
+/**
+ * 显示Usage界面
+ * php app_server.php start|stop|reload
+ */
+Swoole\Network\Server::start(function ()
+{
+    $AppSvr = new EchoServer();
+    $server = Swoole\Network\Server::autoCreate('0.0.0.0', 9505);
+    $server->setProtocol($AppSvr);
+    $server->run(array('worker_num' => 1));
+});
+
