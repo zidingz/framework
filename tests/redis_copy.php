4237c45705d084ca2bb79dfb31aae8ac26b0e2e1
matyhtf <mikan.tenny@gmail.com>
2016-06-03 18:31:12 +0800

    更新RedisCopy脚本

--- tests/redis_copy.php
+++ tests/redis_copy.php
@@ -4,4 +4,18 @@ define('WEBPATH', realpath(__DIR__ . '/..'));
 //包含框架入口文件
 require WEBPATH . '/libs/lib_config.php';
 
+if (empty($argv[2]))
+{
+    echo "php redis_copy.php [aof_file] [dst redis server].\n";
+    echo "etc: php redis_copy.php /data/redis_new_slave/appendonly.aof tcp://192.168.1.73:6001";
+    die;
+}
+
+$aof_file = $argv[1];
+$redis_server = $argv[2];
+if (!is_file($aof_file))
+{
+    echo "redis aof file[$aof_file] not exist.";
+}
+
 Swoole\Redis::syncFromAof('/data/redis_new_slave/appendonly.aof', 'tcp://192.168.1.73:6001');
