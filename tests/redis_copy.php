bcd651df1bae6eca8469206aacda1d216dfa2ed9
matyhtf <mikan.tenny@gmail.com>
2016-06-03 19:08:00 +0800

    增加Redis数据同步脚本

--- tests/redis_copy.php
+++ tests/redis_copy.php
@@ -7,7 +7,7 @@ require WEBPATH . '/libs/lib_config.php';
 if (empty($argv[2]))
 {
     echo "php redis_copy.php [aof_file] [dst redis server].\n";
-    echo "etc: php redis_copy.php /data/redis_new_slave/appendonly.aof tcp://192.168.1.73:6001";
+    echo "etc: php redis_copy.php /data/redis_new_slave/appendonly.aof tcp://192.168.1.73:6001\n";
     die;
 }
 
@@ -15,7 +15,6 @@ $aof_file = $argv[1];
 $redis_server = $argv[2];
 if (!is_file($aof_file))
 {
-    echo "redis aof file[$aof_file] not exist.";
+    echo "redis aof file[$aof_file] not exist.\n";
 }
-
-Swoole\Redis::syncFromAof('/data/redis_new_slave/appendonly.aof', 'tcp://192.168.1.73:6001');
+Swoole\Redis::syncFromAof($aof_file, $redis_server);
