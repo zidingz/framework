d3ae91805e884034b0bc35a3f50673b4141884fc
matyhtf <mikan.tenny@gmail.com>
2016-06-13 15:55:02 +0800

    兼容PHP5和PHP7

--- libs/factory/db.php
+++ libs/factory/db.php
@@ -1,14 +1,14 @@
 <?php
 global $php;
-$config = $php->config['db'][$php->factory_key];
-if (empty($config))
+$configs = $php->config['db'];
+if (empty($configs[$php->factory_key]))
 {
     throw new Swoole\Exception\Factory("db->{$php->factory_key} is not found.");
 }
-$config_use_proxy = $php->config['db'][$php->factory_key]['use_proxy'];
-if (!empty($config_use_proxy))
+$config = $configs[$php->factory_key];
+if (!empty($config['use_proxy']))
 {
-    $db = new Swoole\Database\Proxy($config_use_proxy);
+    $db = new Swoole\Database\Proxy($config);
 }
 else
 {
