3e2830c8345a97ab0e47c9d7383c8570e3d4df95
Don <don@cyberbox.org>
2016-08-22 17:19:23 +0800

    event配置项判断

--- libs/factory/event.php
+++ libs/factory/event.php
@@ -5,4 +5,8 @@ if (empty($config) or !isset($config['async']))
 {
     throw new Exception("require event[$php->factory_key] config.");
 }
+if ($config['async'] && empty($config['type']))
+{
+    throw new Exception("\"type\" config required in event aysnc mode");
+}
 return new Swoole\Component\Event($config);
