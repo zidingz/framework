8f7215b0307c06d4430fb55a3a37ac57d08f23a5
matyhtf <mikan.tenny@gmail.com>
2016-10-18 17:12:33 +0800

    SOA客户端增加等待接收状态

--- libs/Swoole/Client/SOA.php
+++ libs/Swoole/Client/SOA.php
@@ -30,7 +30,11 @@ class SOA
     protected $haveSockets = false;
 
     const OK = 0;
-    const VERSION = 1001;
+
+    /**
+     * 版本号
+     */
+    const VERSION = 1002;
 
     protected static $_instances = array();
 
@@ -222,6 +226,7 @@ class SOA
             unset($retObj->socket);
             return false;
         }
+        $retObj->code = SOA_Result::ERR_RECV;
         //加入wait_list
         $this->waitList[$retObj->id] = $retObj;
         return true;
@@ -803,12 +808,15 @@ class SOA_Result
     const ERR_CONNECT    = 8002; //连接服务器失败
     const ERR_TIMEOUT    = 8003; //服务器端超时
     const ERR_SEND       = 8004; //发送失败
+
     const ERR_SERVER     = 8005; //server返回了错误码
     const ERR_UNPACK     = 8006; //解包失败了
 
     const ERR_HEADER     = 8007; //错误的协议头
     const ERR_TOOBIG     = 8008; //超过最大允许的长度
     const ERR_CLOSED     = 8009; //连接被关闭
+
+    const ERR_RECV       = 8010; //等待接收数据
 
     function __construct($soa_client)
     {
