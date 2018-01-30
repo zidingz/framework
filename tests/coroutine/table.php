3f0b2cf99520a360f0df8056acce74e68a3b3951
matyhtf <mikan.tenny@gmail.com>
2018-01-30 18:04:42 +0800

    路径错误

--- tests/coroutine/table.php
+++ tests/coroutine/table.php
@@ -1,6 +1,6 @@
 <?php
 define('DEBUG', 'on');
-define('WEBPATH', realpath(__DIR__ . '/..'));
+define('WEBPATH', realpath(__DIR__ . '/../../'));
 //包含框架入口文件
 require WEBPATH . '/libs/lib_config.php';
 
