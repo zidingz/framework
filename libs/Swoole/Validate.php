cde2af25cfc39dd329c20d73939a87ce82fa9864
shiguangqi2008@gmail.com <shiguangqi2008@gmail.com>
2018-07-12 16:30:48 +0800

    fix ip valid method

--- libs/Swoole/Validate.php
+++ libs/Swoole/Validate.php
@@ -185,22 +185,7 @@ class Validate
      */
     static function ip($value)
     {
-        $arr = explode('.', $value);
-        if (count($arr) != 4)
-        {
-            return false;
-        }
-        //第一个和第四个不能为0或255
-        if (($arr[0] < 1 or $arr[0] > 254) or ($arr[3] < 1 or $arr[3] > 254))
-        {
-            return false;
-        }
-        //中间2个可以为0,但不能超过254
-        if (($arr[1] < 0 or $arr[1] > 254) or ($arr[2] < 0 or $arr[2] > 254))
-        {
-            return false;
-        }
-        return true;
+        return filter_var($value,FILTER_VALIDATE_IP) !== false;
     }
 
     /**
