c8000e4f15ee6087d02b6634c02f60a368629473
shiguangqi <shiguangqi2008@gmail.com>
2019-02-13 14:34:53 +0800

    add coro idle connection control

--- libs/Swoole/Coroutine/Component/Base.php
+++ libs/Swoole/Coroutine/Component/Base.php
@@ -17,6 +17,7 @@ abstract class Base
     protected $current_entity = 0;
     static $threshold_percent = 1.3;
     static $threshold_num = 10;
+    static $threshold_idle_sec = 120;
 
     function __construct($config)
     {
@@ -25,7 +26,7 @@ abstract class Base
             throw new Swoole\Exception\InvalidParam("require object_id");
         }
         $this->config = $config;
-        $this->pool = new \SplQueue();
+        $this->pool = new MinHeap();
         $this->type .= '_'.$config['object_id'];
     }
 
@@ -35,10 +36,19 @@ abstract class Base
         {
             if ($this->pool->count() > 0)
             {
-                $object = $this->pool->pop();
+                $heap_object = $this->pool->extract();
+                $object = $heap_object['obj'];
+                $time = $heap_object['priority'];
+                //判断空闲时间是否大于配置时间
+                if (time() - $time >= self::$threshold_idle_sec)
+                {
+                    unset($object);
+                    continue;
+                }
                 //必须要 Swoole 2.1.1 以上版本
                 if (property_exists($object, "connected") and $object->connected === false)
                 {
+                    unset($object);
                     continue;
                 }
             }
@@ -64,7 +74,7 @@ abstract class Base
         if ($object)
         {
             if ($this->isReuse()) {
-                $this->pool->push($object);
+                $this->pool->insert(['priority' => time(), 'obj' => $object]);
             }
             Context::delete($this->type);
         }
@@ -91,4 +101,20 @@ abstract class Base
     }
 
     abstract function create();
+}
+
+
+class MinHeap extends \SplHeap
+{
+    /*
+     * key => obj
+     *
+     * */
+    public function compare($array1, $array2)
+    {
+        $p1 = $array1['priority'];
+        $p2 = $array2['priority'];
+        if ($p1 === $p2) return 0;
+        return $p1 > $p2 ? -1 : 1;
+    }
 }
